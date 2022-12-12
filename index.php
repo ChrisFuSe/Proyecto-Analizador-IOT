01259a2ef0c474d5a78f85350271bce6dfb3729a
Christian_FuSE <cfs.christianfuentes@gmail.com>
2022-12-12 05:37:14 -0600

    Insertar en base de datos

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php 
     include 'funciones/conexionDB.php';
-    $mail = $_POST['email'];
-    $pass = $_POST['pass'];
+    $certificado = $_POST['certificado'];
+    $latitud = $_POST['latitud'];
+    $longitud = $_POST['longitud'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $sensor = $_POST['sensor'];
+    $directorio = $_POST['directorio'];
+
+    $query = "INSERT INTO imagenes (certificado, latitud, longitud fecha, hora, directorio, sensor) VALUES ($certificado, $latitud, $longitud, $fecha, $hora, $sensor, $directorio);";
+
+    $db->query($query);
 
     echo $pass;
 ?>
